641f33318acb35e0bd4032602e4a50ef61a3f725
Dilan_GC <132763829+jeffrybrenesUNA@users.noreply.github.com>
2024-05-05 12:29:44 -0600

    Backend de agregar usuario listo
    sadas

--- Backend/Entidades/Request/ReqIngresarImagen.cs
+++ Backend/Entidades/Request/ReqIngresarImagen.cs
@@ -1,9 +1,4 @@
-﻿using Backend.Entidades.Request;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace Backend.Entidades
 {
---
+++ Backend/Logica/LogicaUsuario.cs
@@ -0,0 +1,65 @@
+﻿using Backend.DataAccess;
+using Backend.Entidades;
+using System;
+namespace Backend.Logica
+{
+    internal class LogicaUsuario
+    {
+        public LogicaSesionValida logicaSesionValida = new LogicaSesionValida();
+        // Registro de nuevo usuario 
+        public ResAgregarUsuario agregarUsuario(ReqAgregarUsuario req)
+        {
+
+            ResAgregarUsuario res = new ResAgregarUsuario();
+            //validamos la sesión haciendo un reqBase con el reqAgregarUsuario
+            ReqBase reqBase = new ReqBase();
+            reqBase.sesion = req.sesion;
+            ResBase resBase = new ResBase();
+            resBase = logicaSesionValida.validarSesion(reqBase);
+
+            if (resBase.tipoRegistro!=1 || !resBase.resultado)
+            {
+                //algo salió mal en la sesión
+                res.resultadoTexto = resBase.resultadoTexto;
+                res.tipoRegistro = resBase.tipoRegistro;
+                res.resultado = resBase.resultado;
+            }
+            else
+            {
+                //validar el registro a ingresar
+                if (String.IsNullOrEmpty(req.usuario.nombre)|| String.IsNullOrEmpty(req.usuario.apellidos)||
+                    String.IsNullOrEmpty(req.usuario.email)|| String.IsNullOrEmpty(req.usuario.password)||
+                    String.IsNullOrEmpty(req.usuario.direccion)|| String.IsNullOrEmpty(req.usuario.telefono)
+                    )
+                {
+                    res.resultadoTexto = "Datos Inconsistentes";
+                    res.tipoRegistro = 3;
+                    res.resultado = false;
+                }
+                else
+                {
+                    // si llega hasta acá es porque todo salió bien hasta el momento
+                    try
+                    {
+                        ConnectionDataContext linq = new ConnectionDataContext();
+                        string descripcion = string.IsNullOrEmpty(req.usuario.direccion) ? "" : req.usuario.direccion;
+                        linq.sp_CrearUsuario(req.usuario.nombre, req.usuario.apellidos
+                            , req.usuario.email, req.usuario.password, req.usuario.direccion, 
+                            req.usuario.telefono, descripcion);
+
+                        res.resultado = true;
+                        res.resultadoTexto = "success";
+                        res.tipoRegistro = 1;
+                    }
+                    catch (Exception e)
+                    {
+                        res.resultadoTexto = "Error de BD";
+                        res.tipoRegistro = 4;
+                        res.resultado = false;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
