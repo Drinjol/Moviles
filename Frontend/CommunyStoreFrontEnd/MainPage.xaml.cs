a4e5e44d48e1edce6b9433e7849f21eca7ed6ddb
Dilan_GC <132763829+jeffrybrenesUNA@users.noreply.github.com>
2024-05-05 18:06:25 -0600

    Avance en maui

--- Frontend/CommunyStoreFrontEnd/MainPage.xaml.cs
+++ Frontend/CommunyStoreFrontEnd/MainPage.xaml.cs
@@ -1,25 +1,19 @@
-﻿namespace CommunyStoreFrontEnd
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+
+namespace CommunyStoreFrontEnd
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void btn_agregar_usuario_Clicked(object sender, EventArgs e)
         {
-            count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
         }
     }
-
 }
