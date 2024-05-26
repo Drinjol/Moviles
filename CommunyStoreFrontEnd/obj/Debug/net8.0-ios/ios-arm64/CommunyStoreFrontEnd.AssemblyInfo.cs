442d223b0c77ad26513f18a6484d04246cc43471
DarkGods04 <111477059+DarkGods04@users.noreply.github.com>
2024-05-26 12:12:12 -0600

    cambio

--- CommunyStoreFrontEnd/obj/Debug/net8.0-ios/ios-arm64/CommunyStoreFrontEnd.AssemblyInfo.cs
+++ CommunyStoreFrontEnd/obj/Debug/net8.0-ios/ios-arm64/CommunyStoreFrontEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommunyStoreFrontEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+bef1def8a555491f84413f70af3b1a4f9766a05c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+6e0e897796a2169876988061edc8979b5706df42")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommunyStoreFrontEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommunyStoreFrontEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
